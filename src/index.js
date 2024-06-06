b680f005b744fa7c6d9cd70842eebbb09b8827aa
Pcjmy <1716045191@qq.com>
2024-06-07 00:14:00 +0800

    feat: useContext

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import {React, createRoot} from "./whichReact";
-import "./index.css";
-import UseCallbackPage from "./custom/UseCallbackPage";
-
+import UseContextPage from "./pages/UseContextPage";
+// import UseCallbackPage from "./custom/UseCallbackPage";
 // import jsx from "./pages/ExamplePage";
 // import TransitionPage from "./pages/TransitionPage";
 // import LifeCyclePage from "./pages/LifeCyclePage";
@@ -10,12 +9,16 @@ import UseCallbackPage from "./custom/UseCallbackPage";
 // import UseMemoPage from "./pages/UseMemoPage";
 // import SuspensePage from "./custom/SuspensePage";
 
+import "./style.css";
+import "./index.css";
+
 // ReactDOM.render(jsx, document.getElementById("root"));
 
 const root = createRoot(document.getElementById("root"));
 
 // root.render(jsx);
 // root.render(<UseMemoPage />);
-root.render(<UseCallbackPage/>);
+// root.render(<UseCallbackPage/>);
+root.render(<UseContextPage />);
 
 console.log("React", React.version); //sy-log
